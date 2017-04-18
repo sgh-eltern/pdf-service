cc2c39f652b2f55f06fee4c6987c2b990045b83a
Steffen Uhlig <steffen@familie-uhlig.net>
2017-04-18 22:55:32 +0200

    Verify that the web app is calling the PDF rendering client

--- spec/unit/pdf_service/web/controller_spec.rb
+++ spec/unit/pdf_service/web/controller_spec.rb
@@ -14,4 +14,30 @@ describe PdfService::Web::Controller do
     get '/'
     expect(last_response).to be_ok
   end
+
+  context 'requesting a PDF' do
+    context 'with a URL that is NOT whitelisted' do
+      let(:url_to_render) { 'http://example.com' }
+
+      it 'responds with a basic auth challenge' do
+        post '/', url: url_to_render
+        expect(last_response.status).to eq(401)
+      end
+    end
+
+    context 'with a whitelisted URL' do
+      let(:url_to_render) { 'http://www.eltern-sgh.de/infocenter/leitfaden/' }
+
+      before do
+        expect_any_instance_of(PdfService::StreamingClient).to receive(:stream).with(url_to_render)
+      end
+
+      it 'responds with a PDF' do
+        post '/', url: url_to_render
+
+        expect(last_response).to be_ok
+        expect(last_response.content_type).to eq('application/pdf')
+      end
+    end
+  end
 end
